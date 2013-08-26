5e15e55581077b4dbf2cee3db5e43c90fdac9b4f
Evan Czaplicki <info@elm-lang.org>
2013-08-26 12:37:39 -0700

    Extract constants and colors to make it easier to tweak

--- src/Display.elm
+++ src/Display.elm
@@ -6,35 +6,47 @@ import Inputs (taskField, taskDelete)
 -- Actually display the entire Todo list.
 display : (Int,Int) -> String -> [Task] -> Element
 display (w,h) fieldContent tasks =
-  let pos = midTopAt (relative 0.5) (absolute 40) in
-  layers [ tiledImage w h "/texture.png"
-         , container w h pos <| flow down [ header
-                                          , inputBar fieldContent
-                                          , flow down (map displayTask tasks) ]
-         ]
+    let pos = midTopAt (relative 0.5) (absolute 40) in
+    layers [ tiledImage w h "/texture.png"
+           , container w h pos <| flow down [ header
+                                            , inputBar fieldContent
+                                            , flow down (map displayTask tasks) ]
+           ]
 
 header : Element
 header = 
     flow down
-      [ width 500 . centered . Text.color (rgb 179 179 179) . Text.height 48 <| toText "todos"
-      , color (rgb 141 125 119) (spacer 500 15)
-      , color (rgb 108 125 119) (spacer 500 1 ) ]
+      [ width todoWidth . centered . Text.color titleColor . Text.height 48 <| toText "todos"
+      , color spacerColor1 (spacer todoWidth 15)
+      , color spacerColor2 (spacer todoWidth 1 ) ]
 
 inputBar : String -> Element
 inputBar fieldContent =
-    let grey = rgb 247 247 247 in
-    color grey . container 500 45 midRight . color grey . width 440 . height 45 <|
-          taskField.field "What needs to be done?" fieldContent
+    color inputColor . container todoWidth 45 midRight .
+    color inputColor . size (todoWidth - leftPadding) 45 <|
+        taskField.field "What needs to be done?" fieldContent
 
 -- Display a specific task.
 displayTask : Task -> Element
 displayTask task =
-    let btn : (Text -> Text) -> Element
-        btn f = container 30 30 middle . text . Text.height 28 . f <| toText "&times;"
-        grey = rgb 230 230 230
-    in 
-        color (rgba 255 255 255 0.9) . container 500 30 midRight <|
-        flow right [ container 410 30 midLeft . text <| toText task.description
-                   , taskDelete.customButton task.id
-                         (btn <| Text.color grey) (btn id) (btn bold)
-                   ]
+    let btn clr = let x = text . Text.height 28 . Text.color clr <| toText "&times;"
+                  in container buttonWidth taskHeight middle x
+        taskWidth = todoWidth - leftPadding - buttonWidth
+    in color taskColor . container todoWidth 30 midRight <|
+       flow right [ container taskWidth taskHeight midLeft . text <| toText task.description
+                  , taskDelete.customButton task.id
+                        (btn inputColor) (btn titleColor) (btn red)
+                  ]
+
+-- Dimensions for easy tweaking
+todoWidth   = 500
+leftPadding = 60
+buttonWidth = 30
+taskHeight  = 30
+
+-- Colors for easy tweaking
+titleColor   = rgb 179 179 179
+spacerColor1 = rgb 141 125 119
+spacerColor2 = rgb 108 125 119
+inputColor   = rgb 247 247 247
+taskColor    = rgba 255 255 255 0.9
